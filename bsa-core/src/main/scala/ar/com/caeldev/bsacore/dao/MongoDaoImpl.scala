81a6473ee1414473e8a1a29b0d6492637a9d4214
Adolfo Ernesto Custidiano Secchi <adolfoecs@gmail.com>
2013-05-24 18:52:47 -0300

    [u] change to abstract class to support scala 2.9.x

--- bsa-core/src/main/scala/ar/com/caeldev/bsacore/dao/MongoDaoImpl.scala
+++ bsa-core/src/main/scala/ar/com/caeldev/bsacore/dao/MongoDaoImpl.scala
@@ -3,15 +3,8 @@ package ar.com.caeldev.bsacore.dao
 import ar.com.caeldev.bsacore.serializer.{ BsonSerializer, Serializer }
 import com.mongodb.DBObject
 
-class MongoDaoImpl[T <: AnyRef](implicit mot: Manifest[T]) extends GenericDao[T] {
+abstract class MongoDaoImpl[T <: AnyRef](implicit mot: Manifest[T]) extends GenericDao[T] {
 
   val serializer: Serializer[T, DBObject] = new BsonSerializer[T]
 
-  def findAll(): List[T] = ???
-
-  def save(entity: T): T = ???
-
-  def remove(entity: T) {}
-
-  def findById(id: Serializable): T = ???
 }
